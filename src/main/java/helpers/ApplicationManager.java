bcb9b77875ae36a305372bc8c94639962273a359
webinartelran <webinartelran@gmail.com>
2023-08-15 00:15:53 +0300

    CW24

---
+++ src/main/java/helpers/ApplicationManager.java
@@ -0,0 +1,19 @@
+package helpers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public interface ApplicationManager {
+
+    WebDriver wd = new ChromeDriver();
+
+    default void init(){
+        wd.navigate().to("https://demoqa.com/");
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+
+}
